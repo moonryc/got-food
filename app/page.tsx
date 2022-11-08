8a9b97b606e03d67e0cc8d93dc2adfd5e23c43de
Ryan Moon <ryanmoondevelopment@gmail.com>
2022-11-07 18:48:17 -0600

    format

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-export default function HomePage(){
-    return(
-            <div>
-                <h1>HELLo</h1>
-                </div>
+export default function HomePage() {
+    return (
+        <div>
+            <h1>HELLo</h1>
+        </div>
     )
 }
